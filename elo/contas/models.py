7cbceb7497cc91304e3d9f71f53456260f661704
VICTOR PONTUAL GUEDES ARRUDA NOBREGA <victorpontualgan@gmail.com>
2026-01-01 12:21:03 -0300

    feat: criar base do models do app contas

--- elo/contas/models.py
+++ elo/contas/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Pesquisador(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    # O campo 'nome' e 'email' podem ser gerenciados pelo modelo User (first_name, last_name, email)
+    titulo = models.CharField(max_length=100, blank=True, null=True)
+    celular = models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
