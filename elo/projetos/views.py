284e279322a1f6baa35f6df6e61370bdf1997316
VICTOR PONTUAL GUEDES ARRUDA NOBREGA <victorpontualgan@gmail.com>
2026-01-14 12:52:58 -0300

    feat(projetos): implementa view de listagem de projetos com autenticação
    - View lista_projetos com @login_required
    - Prefetch de participações e pesquisadores
    - Configuração de URLs

--- elo/projetos/views.py
+++ elo/projetos/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Projeto
 
-# Create your views here.
+@login_required
+def lista_projetos(request):
+    projetos = Projeto.objects.prefetch_related('participacao_set__pesquisador__user').all()
+    return render(request, 'projetos/lista_projetos.html', {'projetos': projetos})
