9720829d859f067674b38b670a2dadc8150849ec
VICTOR PONTUAL GUEDES ARRUDA NOBREGA <victorpontualgan@gmail.com>
2026-01-27 11:08:03 -0300

    feat: adicionar logout

--- elo/contas/urls.py
+++ elo/contas/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from .views import UserLoginView, UserCadastroView
 
 urlpatterns = [
     path('', UserLoginView.as_view(), name='login'),
     path('cadastro/', UserCadastroView.as_view(), name='cadastro'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
